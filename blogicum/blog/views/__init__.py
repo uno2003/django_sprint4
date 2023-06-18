83d244d301eca8d92855611e9d4589bb16118e66
Mikhail <uno2003@mail.ru>
2023-06-18 19:00:13 +0300

    First review

--- blogicum/blog/views/__init__.py
+++ blogicum/blog/views/__init__.py
@@ -4,3 +4,19 @@ from blog.views.users import (UserLoginView, UserLogoutView,
                               UserEditView, UserProfileView)
 from blog.views.comments import (CommentCreateView, CommentEditView,
                                  CommentDeleteView)
+
+__all__ = (
+    'IndexView',
+    'PostDetailView',
+    'CategoryPostView',
+    'PostCreateView',
+    'PostUpdateView',
+    'PostDeleteView',
+    'UserLoginView',
+    'UserLogoutView',
+    'UserEditView',
+    'UserProfileView',
+    'CommentCreateView',
+    'CommentEditView',
+    'CommentDeleteView'
+)
