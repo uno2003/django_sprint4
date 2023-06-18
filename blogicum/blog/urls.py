2867adbe162045ba80bb433c46ded7b052b72ddb
Mikhail <uno2003@mail.ru>
2023-06-18 19:16:06 +0300

    First review

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import (IndexView, PostDetailView, CategoryPostView,
-                        PostCreateView, PostUpdateView, PostDeleteView,
-                        UserLoginView, UserLogoutView, UserEditView,
-                        UserProfileView, CommentCreateView, CommentEditView,
-                        CommentDeleteView)
+from blog.views.posts import (IndexView, PostDetailView, CategoryPostView,
+                              PostCreateView, PostUpdateView, PostDeleteView)
+from blog.views.users import (UserLoginView, UserLogoutView,
+                              UserEditView, UserProfileView)
+from blog.views.comments import (CommentCreateView, CommentEditView,
+                                 CommentDeleteView)
 
 app_name = 'blog'
 
