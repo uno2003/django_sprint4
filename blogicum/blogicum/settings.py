bc97a0448863b23215d426dcf1f98a204bc3c5a5
Mikhail <uno2003@mail.ru>
2023-06-18 12:19:10 +0300

    First review

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dz+7$5k-g%8ad83qubaja6qt(n7#*$0(v$ld*ns#(5cxgut(25'
-
+#SECRET_KEY = 'django-insecure-dz+7$5k-g%8ad83qubaja6qt(n7#*$0(v$ld*ns#(5cxgut(25'
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
